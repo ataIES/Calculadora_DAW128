8a7c5f16b52777062274a0541d749c2c91666cb5
ataIES <adrian.tresgalloarozamena@iesmiguelherrero.com>
2022-12-21 15:38:13 +0100

    Ejercicio2_Adrian

---
+++ Calculadora_AdrianTresgallo/src/main/java/tema4/calculadora_adriantresgallo/Calculadora_AdrianTresgallo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package tema4.calculadora_adriantresgallo;
+
+/**
+ *
+ * @author DAW128
+ */
+public class Calculadora_AdrianTresgallo {
+
+    public static void main(String[] args) {
+        System.out.println("Bienvenidos!!!");
+    }
+}
